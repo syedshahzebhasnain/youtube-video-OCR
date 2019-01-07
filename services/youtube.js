e841846d6f86d34d4e424e56151dd57d5e6a51f3
ryzen <syed.hasnain@dal.ca>
2019-01-06 23:13:16 -0400

    initial

---
+++ services/youtube.js
@@ -0,0 +1,14 @@
+require('youtube-frames');
+module.exports = {
+    async downloadAndGenerateScreenShots(videoId) {
+        try {
+            const video = await $ytvideo(`https://www.youtube.com/watch?v=${videoId}`, `${videoId}-SC`);
+            let result = video.download(`./frames/${videoId}`).toFrames()
+            await result.prom
+            return true
+        } catch (err) {
+            console.log(err)
+            return false
+        }
+    }
+}
